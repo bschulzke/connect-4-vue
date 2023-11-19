fa56c730d04a1f79b709400a3811611ba74d2023
Ben Schulzke <benschulzke@gmail.com>
2023-11-19 15:54:58 -0700

    fix vue config reppo name for production environment

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/connect-4-vue/" : "/",
 }
